1ebdc516e1b11187f4b6299139331bc9435d388d
Abhi1922shek <abhiduggodi1922@gmail.com>
2024-07-20 11:58:04 +0530

    Vendor profile form & validation updated

--- vendor/forms.py
+++ vendor/forms.py
@@ -1,7 +1,9 @@
 from django import forms
 from vendor.models import Vendor
+from accounts.validators import allowImagesValidator
 
 class VendorForm(forms.ModelForm):
+    vendor_license = forms.FileField(widget=forms.FileInput(attrs={'class':'btn btn-info'}), validators=[allowImagesValidator])
     class Meta:
         model = Vendor
         fields = ['vendor_name', 'vendor_license']
